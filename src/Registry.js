0341959f61e7dffa1bbc51b135dd67ea2a821e5a
SoggyPiggy <soggypiggy@gmail.com>
2019-01-18 22:12:17 -0500

    Update registry

--- src/Registry.js
+++ src/Registry.js
@@ -1,2 +1,34 @@
+const createGroup = function createCommandGroup({ id = null, name = null }) {
+  return {
+    id,
+    name,
+    commands: new Map(),
+  }
+}
+
 module.exports = class Registry {
+  constructor() {
+    this.commands = new Map();
+    this.groups = new Map([[null, createGroup()]]);
+  }
+
+  createGroup({id, name}) {
+    if (typeof id !== 'string') throw new Error('Command group id needs to be string');
+    if (typeof name !== 'string') throw new Error('Command group name needs to be string');
+    if (this.groups.has(id)) throw new Error(`Command group id '${id}' is already in use`);
+    const group = createGroup({ id, name });
+    this.groups.set(group.id, group);
+    return this;
+  }
+  
+  createGroups(groups = []) {
+    groups.forEach((group) => {
+      try {
+        this.createGroup(group);
+      } catch (error) {
+        console.warn(error);
+      }
+    });
+    return this;
+  }
 }
