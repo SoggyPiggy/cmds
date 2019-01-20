c736c9495396f1baf53dfd3ca1b11a670824ecbb
SoggyPiggy <soggypiggy@gmail.com>
2019-01-20 00:31:58 -0500

    Update Command

--- src/Command.js
+++ src/Command.js
@@ -1,2 +1,29 @@
+const genAliases = function generateAliasesFromAliasesAndID(id, aliases) {
+  return [...Set([
+    ...aliases.map(alias => alias.toLowerCase()),
+    ...aliases.map(alias => alias.toLowerCase().replace(/-/g, '')),
+    id.toLowerCase(),
+    id.toLowerCase().replace(/-/g, ''),
+  ])];
+};
+
 module.exports = class Command {
+  constructor({
+    id = null,
+    aliases = [],
+    description = '',
+    details = '',
+    examples = [],
+    args = [],
+    run = message => message.reply(`Command '${id}' run not set up properly`),
+  }) {
+    if (typeof id !== 'string') throw new Error('Command must have string id');
+    this.id = id.toLowerCase();
+    this.aliases = genAliases(id, aliases);
+    this.description = description;
+    this.details = details;
+    this.examples = examples;
+    this.args = args;
+    this.run = run;
+  }
 };
