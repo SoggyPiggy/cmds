ae8dfa7ce384715e6e4f9ecd255165b9d3985bfd
SoggyPiggy <soggypiggy@gmail.com>
2019-01-20 20:42:38 -0500

    Update Cmds Added a prefixes option

--- src/Cmds.js
+++ src/Cmds.js
@@ -3,9 +3,11 @@ const { Command } = require('./modules');
 
 module.exports = class Cmds extends EventEmitter {
   constructor(discord, {
+    prefix = ['>'],
   }) {
     super();
     this.discord = discord;
+    this.prefixes = Array.isArray(prefix) ? prefix : [prefix];
   }
 
   static get Command() {
