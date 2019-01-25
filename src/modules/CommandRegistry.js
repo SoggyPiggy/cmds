1de15c457c72d51f30c7ac9f1be8e5486095a322
SoggyPiggy <soggypiggy@gmail.com>
2019-01-24 23:32:34 -0500

    Update CommandRegistry Need to fix registerCommand to use commakgAliases

--- src/modules/CommandRegistry.js
+++ src/modules/CommandRegistry.js
@@ -1,3 +1,5 @@
+const Command = require('./Command');
+
 const createGroup = function createCommandGroup({ id = null, name = null }) {
   return {
     id,
@@ -10,6 +12,7 @@ module.exports = class CommandRegistry {
   constructor(cmds) {
     this.cmds = cmds;
     this.commands = new Map();
+    this.commandAliases = new Set();
     this.groups = new Map([[null, createGroup()]]);
   }
 
@@ -30,5 +33,29 @@ module.exports = class CommandRegistry {
       }
     });
     return this;
+  }
+
+  registerCommand(constructor) {
+    if (typeof constructor !== 'function') throw new Error('Cannot register improper command constructor');
+    const command = new constructor();
+    if (command instanceof Command) throw new Error('Command constructor did not produce Command instance');
+    command.registerCmds(this.cmds);
+    if (!this.groups.has(command.group)) this.createGroup({ id: command.group });
+    const group = this.groups.get(command.group);
+    command.aliases.forEach((alias) => {
+      if (this.commands.has(alias)) throw new Error(`Command with '${alias}' alias already registered`);
+      else {
+        this.commands.set(alias, command);
+        group.commands.set(alias, command);
+      }
+    });
+  }
+
+  createCommand(options = {}) {
+    this.registerCommand(class extends Command {
+      constructor() {
+        super(options);
+      }
+    });
   }
 };
