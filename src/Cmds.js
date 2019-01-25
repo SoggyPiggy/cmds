f780b84e32ecf79ae091e3d4cbb0256322f598be
SoggyPiggy <soggypiggy@gmail.com>
2019-01-25 02:04:09 -0500

    Update Cmds Added the initial functions that'll be used for parsing commands

--- src/Cmds.js
+++ src/Cmds.js
@@ -1,6 +1,20 @@
 const { EventEmitter } = require('events');
 const { Command, CommandRegistry } = require('./modules');
 
+const isCommand = function isMessageACommand(message) {
+  // TODO: Add check whether message received is a command
+};
+
+const parseCommand = function parseMessageIntoUsableStrings(message) {
+  // TODO: Add the logic is parsing the string
+};
+
+const command = async function beginCommandProcess(message) {
+  if (!isCommand(message)) return;
+  const [raw, command, parameters] = parseCommand(message.content);
+  // TODO: Finish off the logic
+};
+
 module.exports = class Cmds extends EventEmitter {
   constructor(discord, {
     prefix = ['>'],
@@ -9,6 +23,8 @@ module.exports = class Cmds extends EventEmitter {
     this.discord = discord;
     this.registry = new CommandRegistry(this);
     this.prefixes = Array.isArray(prefix) ? prefix : [prefix];
+
+    this.discord.on('message', message => isCommand(message));
   }
 
   static get Command() {
