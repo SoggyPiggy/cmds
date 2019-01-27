0f0e90ef7c6b057f9e453c44e442cd10ad743c47
SoggyPiggy <soggypiggy@gmail.com>
2019-01-26 22:46:21 -0500

    Update Cmds removed seperate functions and combined into one class method`

--- src/Cmds.js
+++ src/Cmds.js
@@ -1,19 +1,6 @@
+const { DMChannel } = require('discord.js');
 const { EventEmitter } = require('events');
 const { Command, CommandRegistry } = require('./modules');
-
-const isCommand = function isMessageACommand(message) {
-  // TODO: Add check whether message received is a command
-};
-
-const parseCommand = function parseMessageIntoUsableStrings(message) {
-  // TODO: Add the logic is parsing the string
-};
-
-const command = async function beginCommandProcess(message) {
-  if (!isCommand(message)) return;
-  const [raw, command, parameters] = parseCommand(message.content);
-  // TODO: Finish off the logic
-};
 
 module.exports = class Cmds extends EventEmitter {
   constructor(discord, {
@@ -22,9 +9,18 @@ module.exports = class Cmds extends EventEmitter {
     super();
     this.discord = discord;
     this.registry = new CommandRegistry(this);
-    this.prefixes = Array.isArray(prefix) ? prefix : [prefix];
+    this.prefixes = [prefix].flat();
 
-    this.discord.on('message', message => isCommand(message));
+    this.discord.on('message', message => this.processMessage(message));
+  }
+
+  async processMessage(message) {
+    const { content, channel } = message;
+    const prefixRegex = new RegExp(`^(${this.prefixes.map(prefix => prefix.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|')})? ?(.+)`);
+    const prefixMatch = prefixRegex.exec(content);
+    if (!(prefixMatch[1] || channel instanceof DMChannel)) return;
+    const [command, operation, argumentString] = prefixMatch[2].match(/(\S?)(?: (.+))/);
+    const arguments = argumentString.match(/(['"]).+\1|\S+/).replace(/^(['"])?(.+?)\1$/, '');
   }
 
   static get Command() {
