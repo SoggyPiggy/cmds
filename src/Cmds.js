8eea46bd8092406b5961d005070953e02d4356af
SoggyPiggy <soggypiggy@gmail.com>
2019-01-26 23:57:45 -0500

    Fix Cmds processMessage Commands are all now properly processed

--- src/Cmds.js
+++ src/Cmds.js
@@ -14,13 +14,13 @@ module.exports = class Cmds extends EventEmitter {
     this.discord.on('message', message => this.processMessage(message));
   }
 
-  async processMessage(message) {
+  processMessage(message) {
     const { content, channel } = message;
     const prefixRegex = new RegExp(`^(${this.prefixes.map(prefix => prefix.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|')})? ?(.+)`);
     const prefixMatch = prefixRegex.exec(content);
     if (!(prefixMatch[1] || channel instanceof DMChannel)) return;
-    const [command, operation, argumentString] = prefixMatch[2].match(/(\S?)(?: (.+))/);
-    const arguments = argumentString.match(/(['"]).+\1|\S+/).replace(/^(['"])?(.+?)\1$/, '');
+    const [command, operation, params] = prefixMatch[2].match(/(\S+)(?: (.+))?/);
+    const parameters = params ? params.match(/(['"]).+\1|\S+/g).map(param => param.replace(/^(['"])?(.+?)\1$/, '')) : [];
   }
 
   static get Command() {
