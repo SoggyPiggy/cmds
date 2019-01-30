d0de385148c51f33a1f80edcce5178bbe0c90d1e
SoggyPiggy <soggypiggy@gmail.com>
2019-01-30 16:57:02 -0500

    Update ParameterType

--- src/modules/ParameterType.js
+++ src/modules/ParameterType.js
@@ -1,2 +1,12 @@
 module.exports = class ParameterType {
+  constructor({
+    id = null,
+    parse = async data => data,
+    validate = async () => true,
+  }) {
+    if (typeof id !== 'string') throw new Error('ParameterType must have string id');
+    this.id = id.toLowerCase();
+    this.parse = parse;
+    this.validate = validate;
+  }
 };
