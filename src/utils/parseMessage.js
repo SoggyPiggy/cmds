30d7cc94f9af7642493a9f7bc475f1ecb1cc0db6
SoggyPiggy <soggypiggy@gmail.com>
2019-01-31 02:15:07 -0500

    Update parseMessage Removed the maybe unneeded isCommand check

--- src/utils/parseMessage.js
+++ src/utils/parseMessage.js
@@ -7,16 +7,6 @@ module.exports = function parseMessage(cmds, message) {
   const isPrefixed = typeof prefix === 'string';
   const isDM = message.channel instanceof DMChannel;
   const isCommand = isPrefixed || isDM;
-  if (!isCommand) {
-    return {
-      isDM,
-      isPrefixed,
-      isCommand,
-      command: null,
-      operation: null,
-      rawArguments: null,
-    };
-  }
   const [command, operation, rawArguments] = prefixMatch[2].match(/(\S+)(?: (.+))?/);
   return {
     isDM,
